f61c68502e478303a3e72caceb047284ad5cc1d1
ChangWon Lee <chiyah92@gmail.com>
2025-04-18 11:52:57 +0900

    Update ssh.nix

--- modules/agenix/ssh.nix
+++ modules/agenix/ssh.nix
@@ -1,9 +1,13 @@
 let
   # Your SSH key
   macbookChangwonleeKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHP1NSXj1F65vAVy4o/oYMh9w77pu8+KnXc16JANnCsB changwon.lee@devsisters.com";
+  macstudioChangwonKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOh9iL7pSgqgbuDvGM9qwCWSBLPX0A6WaMMmR8R2WHB chiyah92@gmail.com";
   
   # Add more keys for other machines if needed
-  allKeys = [ macbookChangwonleeKey ];
+  allKeys = [
+    macbookChangwonleeKey
+    macstudioChangwonKey
+  ];
 in
 {
   # rclone configuration file
