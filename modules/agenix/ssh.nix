61beddb1a98f8b0eb57f7eebee7a4435385687f4
ChangWon Lee <cs.changwon.lee@gmail.com>
2025-04-30 01:42:14 +0900

    remove redundancy

--- modules/agenix/ssh.nix
+++ modules/agenix/ssh.nix
@@ -1,17 +1,10 @@
 let
   # Your SSH key - Using identity from ~/.ssh/ directory
-  # This tries multiple possible keys to increase chances of success
-  sshKeysFromIdentity = [
-    # Public keys that should match your private key
+  sshKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHP1NSXj1F65vAVy4o/oYMh9w77pu8+KnXc16JANnCsB changwon.lee@devsisters.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOh9iL7pSgqgbuDvGM9qwCWSBLPX0A6WaMMmR8R2WHB chiyah92@gmail.com"
-    # Add more keys as needed
   ];
-
-  # Use all keys for maximum compatibility
-  allKeys = sshKeysFromIdentity;
 in
 {
-  # rclone configuration file - accessible by any of the keys
-  "modules/agenix/rclone.conf.age".publicKeys = allKeys;
+  "modules/agenix/rclone.conf.age".publicKeys = sshKeys;
 }
