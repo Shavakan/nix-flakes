e7663f52c909caa521fe7e12b4c3254c09546235
ChangWon Lee <cs.changwon.lee@gmail.com>
2025-07-09 19:48:46 +0900

    darwin: switch realforce key mapping on macbook

--- modules/darwin/macbook.nix
+++ modules/darwin/macbook.nix
@@ -11,6 +11,31 @@
     dock = {
       autohide = true;
     };
+
+    # External keyboard configuration for MacBook only
+    CustomUserPreferences = {
+      # REALFORCE HYBRID US TKL keyboard mapping (VendorID: 2131, ProductID: 771)
+      "com.apple.keyboard.modifiermapping.2131-771-0" = {
+        # Swap Command and Option/Alt keys on Realforce keyboard
+        "0" = {
+          "HIDKeyboardModifierMappingSrc" = 30064771072; # Command key
+          "HIDKeyboardModifierMappingDst" = 30064771078; # Option/Alt key
+        };
+        "1" = {
+          "HIDKeyboardModifierMappingSrc" = 30064771078; # Option/Alt key  
+          "HIDKeyboardModifierMappingDst" = 30064771072; # Command key
+        };
+        # Swap Control and Caps Lock on Realforce keyboard
+        "2" = {
+          "HIDKeyboardModifierMappingSrc" = 30064771296; # Left Control
+          "HIDKeyboardModifierMappingDst" = 30064771129; # Caps Lock
+        };
+        "3" = {
+          "HIDKeyboardModifierMappingSrc" = 30064771129; # Caps Lock
+          "HIDKeyboardModifierMappingDst" = 30064771296; # Left Control  
+        };
+      };
+    };
   };
 
   # MacBook-specific LaunchAgents
